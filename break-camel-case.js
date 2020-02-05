7e7fcba2c27e507db5449fb34ac7bde1ef612fa6
minheekangg <minheekangg@gmail.com>
2020-02-04 22:48:13 -0500

    day 1 done

---
+++ break-camel-case.js
@@ -0,0 +1,22 @@
+// Complete the solution so that the function will break up camel casing, using a space between words.
+
+//     Example
+// solution('camelCasing') // => should return 'camel Casing'
+
+// Test.assertEquals(solution('camelCasing'), 'camel Casing', 'Unexpected result')
+// Test.assertEquals(solution('camelCasingTest'), 'camel Casing Test', 'Unexpected result')
+
+function solution(string) {
+    string = string.split('').map(function (el) {
+        if (el === el.toUpperCase()) {
+            el = ' ' + el
+        }
+        return el
+    })
+    return string.join('')
+}
+
+// better solution:
+function solution(string) {
+    return string.replace(/[A-Z]/g, function (c) { return " " + c; });
+}
