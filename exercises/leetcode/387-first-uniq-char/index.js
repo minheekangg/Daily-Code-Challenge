12ea18cc7dade310bccfae69885ee851e694f324
minheekangg <minheekangg@gmail.com>
2020-07-23 14:06:15 -0400

    added notable solutions

--- exercises/leetcode/387-first-uniq-char/index.js
+++ exercises/leetcode/387-first-uniq-char/index.js
@@ -15,25 +15,48 @@
     return 2.
  */
 
-// My Solution
-/**
- * @param {string} s
- * @return {number}
- */
-var firstUniqChar = function (s) {
-    let result = [];
-    let seen = {};
 
-    for (let idx in s) {
-        const char = s[idx];
-        if (!seen[char]) {
-            seen[char] = idx;
-            result.push(idx);
-        } else if (!!seen[char]) {
-            result = result.filter(e => e !== seen[char]);
+
+// NOTABLE SOLUTION 1:
+var firstUniqChar = function (s) {
+    var sMap = new Map();
+    for (let i = 0; i < s.length; i++) {
+        if (sMap.has(s[i])) {
+            sMap.set(s[i], 2);  // duplicated character: value set 2   
+        } else {
+            sMap.set(s[i], 1); // unique character: value set 1
         }
+    };
+    //e.g. s= "ackcamkcg",  sMap= {a => 2, c => 2, k => 2, m => 1, g => 1}
+    for (let i = 0; i < s.length; i++) {
+        if (sMap.get(s[i]) === 1) return i;
     }
-    return result[0] === undefined ? -1 : parseInt(result[0], 10);
-};
+    return -1;
+}
+
+// NOTABLE SOLUTION 2:
+var firstUniqChar = function (s) {
+    for (let i = 0; i < s.length; i++) {
+        if (s.indexOf(s[i]) == s.lastIndexOf(s[i])) return i;
+    }
+    return -1;
+}
+
+// My Solution
+// var firstUniqChar = function (s) {
+//     let result = [];
+//     let seen = {};
+
+//     for (let idx in s) {
+//         const char = s[idx];
+//         if (!seen[char]) {
+//             seen[char] = idx;
+//             result.push(idx);
+//         } else if (!!seen[char]) {
+//             result = result.filter(e => e !== seen[char]);
+//         }
+//     }
+//     return result[0] === undefined ? -1 : parseInt(result[0], 10);
+// };
 
 module.exports = firstUniqChar;
